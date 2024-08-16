b0f06173fc45a69e7c74bf986823b0eafefe1c54
Kieran Speight <kieranjspeight@gmail.com>
2024-08-16 19:40:41 +0100

    Rotation Constraints and Misc. Changes
    Added constraints to rotation and fixed up prefab/ rigidbody settings.

--- Assets/Scripts/Pufferfish.cs
+++ Assets/Scripts/Pufferfish.cs
@@ -5,8 +5,6 @@ using UnityEngine.Rendering;
 
 public class Pufferfish : MonoBehaviour
 {
-    public Transform target;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +14,6 @@ public class Pufferfish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        
     }
 }
