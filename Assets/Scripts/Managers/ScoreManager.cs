9376f7a8c015c5cd4b4dcee87561a2602d2260f5
shivani-520 <shivani_2002@icloud.com>
2024-08-17 21:09:59 +0100

    Score Manager
    Added the collectables text which updates as the puffer collects them.

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    [Header("Timer Logic")]
     public TextMeshProUGUI timerText;
-
     public TextMeshProUGUI timerTextWhenCompleted;
+    public TextMeshProUGUI secondsText;
+    public TextMeshProUGUI minutesText;
 
     private float currentTime;
     private float startingTime;
+
+    [Header("Collectables Logic")]
+    public TextMeshProUGUI collectablesText;
+    public TextMeshProUGUI collectablesTextWhenCompleted;
+
 
     private void Start()
     {
@@ -19,16 +26,48 @@ public class ScoreManager : MonoBehaviour
 
     private void Update()
     {
-        // During the game
-        currentTime += Time.deltaTime;
-        timerText.text = currentTime.ToString("0.00");
-
-        // When level has been completed
-        timerTextWhenCompleted.text = currentTime.ToString("0.00");
+        Timer();
 
         if (GameManager.Instance.hasCompletedLevel)
         {
+            collectablesText.gameObject.SetActive(false);
             timerText.gameObject.SetActive(false);
         }
+    }
+
+    private void Timer()
+    {
+        // During the game
+        currentTime += Time.deltaTime;
+
+        // Checks if the current time is 60 seconds or less 
+        if (currentTime < 60f)
+        {
+            // Format time as seconds
+            timerText.text = currentTime.ToString("0.00");
+            timerTextWhenCompleted.text = currentTime.ToString("0.00");
+        }
+        else
+        {
+            // When time exceeds 60 seconds, format time in minutes and seconds
+            float minutes = Mathf.Floor(currentTime / 60);
+            float seconds = currentTime % 60;
+            timerText.text = string.Format("{0}:{1:00}", minutes, seconds);
+            timerTextWhenCompleted.text = string.Format("{0}:{1:00}", minutes, seconds);
+
+            // Change default "seconds" text to "minutes"
+            minutesText.gameObject.SetActive(true);
+            secondsText.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void Collectables(int collectables)
+    {
+        // During the game
+        collectablesText.text = collectables.ToString();
+
+        // When level has been completed
+        collectablesTextWhenCompleted.text = collectables.ToString();
     }
 }
--- Assets/Scripts/Player/Collection.cs
+++ Assets/Scripts/Player/Collection.cs
@@ -6,11 +6,18 @@ public class Collection : MonoBehaviour
     public AudioSource source;
     public AudioClip clip;
 
+    public ScoreManager scoreManager;
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Collectible"))
         {
             collectibles++;
+            if(scoreManager != null)
+            {
+                scoreManager.Collectables(collectibles);
+            }
+
             source.PlayOneShot(clip);
             Destroy(other.gameObject);
         }
