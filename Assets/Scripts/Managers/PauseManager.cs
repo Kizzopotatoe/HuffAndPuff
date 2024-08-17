0e0dea53113590a34aaeea5d30e638fe56913748
shivani-520 <shivani_2002@icloud.com>
2024-08-17 17:44:09 +0100

    Pause Menu
    resume, options, controls, main menu buttons

---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public GameObject pauseButton;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        //pauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        //pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
