6744739dc7687a529be951c1c295a36e0c72cf48
Kieran Speight <kieranjspeight@gmail.com>
2024-08-17 15:34:55 +0100

    Collectible Audio and Models Added

--- Assets/Scripts/Player/Collection.cs
+++ Assets/Scripts/Player/Collection.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class Collection : MonoBehaviour
 {
     public int collectibles = 0;
+    public AudioSource source;
+    public AudioClip clip;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Collectible"))
         {
             collectibles++;
+            source.PlayOneShot(clip);
             Destroy(other.gameObject);
         }
     }
