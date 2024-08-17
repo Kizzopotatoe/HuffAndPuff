c01dfb70cec7fc0c5188b58fb374ff7fe99ec3c5
Kieran Speight <kieranjspeight@gmail.com>
2024-08-17 14:40:55 +0100

    Swimming Test

---
+++ Assets/Scripts/WaterPhysics/Swimming.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swimming : MonoBehaviour
+{
+    private Rigidbody rb;       //Reference to the players rigidbody component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = FindObjectOfType<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Water"))
+        {
+            rb.useGravity = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Water"))
+        {
+            rb.useGravity = true;
+        }
+    }
+}
