5432e7aecac4747cfedf909d60c239ca5622c5fb
shivani-520 <shivani_2002@icloud.com>
2024-08-19 16:17:17 +0100

    Explosion Updates

--- Assets/Scripts/Obstacles/BrokenFloor.cs
+++ Assets/Scripts/Obstacles/BrokenFloor.cs
@@ -6,7 +6,6 @@ public class BrokenFloor : MonoBehaviour
     public AudioSource source;
     public AudioClip clip;
 
-    public GameObject crumblingEffect;
     public GameObject explosionEffect;
 
     //If the player collides with this object, and they are over a specific size, it will be destroyed
@@ -15,7 +14,6 @@ public class BrokenFloor : MonoBehaviour
         if(other.gameObject.CompareTag("Player"))
         {
             var pufferfish = other.gameObject.GetComponent<Pufferfish>();
-            crumblingEffect.SetActive(true);
 
             if (pufferfish.power >= 12f)
             {
@@ -25,19 +23,6 @@ public class BrokenFloor : MonoBehaviour
                 Destroy(explosionEffectPrefab, 2f);
 
                 Destroy(this.gameObject);
-            }
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            var pufferfish = collision.gameObject.GetComponent<Pufferfish>();
-
-            if (pufferfish.power <= 12f)
-            {
-                crumblingEffect.SetActive(false);
             }
         }
     }
